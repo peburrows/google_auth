e2a543ac89c01ff0eacc3dddcc5596b47256574a
Kian-Meng Ang <kianmeng@cpan.org>
2025-01-16 15:24:10 +0800

    Support Elixir 1.18 in GitHub CI
    We also bump Credo to resolve warnings on missing functions.

--- mix.exs
+++ mix.exs
@@ -42,6 +42,7 @@ defmodule Goth.Mixfile do
       {:jose, "~> 1.11"},
       {:jason, "~> 1.1"},
       {:finch, "~> 0.17"},
+      {:castore, "~> 1.0", only: :test},
       {:bypass, "~> 2.1", only: :test},
       {:ex_doc, "~> 0.19", only: :dev},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
